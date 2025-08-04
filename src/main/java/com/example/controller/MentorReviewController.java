16ab84f6f968b5133ad3513d30ad7ce75472211d
GY <gy@example.com>
2025-08-04 15:04:53 +0900

    Merge branch 'master' of https://github.com/rocswc/moonpull.git

--- src/main/java/com/example/controller/MentorReviewController.java
+++ src/main/java/com/example/controller/MentorReviewController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,7 +10,7 @@ import com.example.service.MentorReviewService;
 
 @RestController
 @RequestMapping("/api/mentor-review")
-@CrossOrigin(origins = "*") // 모든 오리진 허용 (테스트용)
+@CrossOrigin(origins = "http://192.168.56.1:8888", allowCredentials = "true") // credentials:true 일 때는 * 안됨!
 public class MentorReviewController {
 
     @Autowired
@@ -19,7 +20,6 @@ public class MentorReviewController {
     @GetMapping("/{mentorId}")
     public List<MentorReviewVO> getReviews(@PathVariable("mentorId") int mentorId) {
         return mentorReviewService.getReviewsByMentorId(mentorId);
-    //d
     }
 
     // 리뷰 등록 및 평균 평점 업데이트
