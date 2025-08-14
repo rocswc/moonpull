b92cf5d94944c24e61ab9ecd12db78d72a51900f
billy4242 <billy4242@naver.com>
2025-08-14 13:12:00 +0900

    Merge branch 'master' of https://github.com/rocswc/moonpull.git

---
+++ src/main/java/com/example/dto/MenteeInfo.java
@@ -0,0 +1,14 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MenteeInfo {
+    private Long id;        // 멘티의 userId
+    private String name;    // 멘티 이름
+    private Integer age;    // 멘티 나이
+}
