76b5ff2b5ec77733d79375a9e10d0779341f43e1
rocswc <gs05047@naver.com>
2025-08-14 09:18:12 +0900

    commit

--- src/main/java/com/example/config/ElasticSearchConfig.java
+++ src/main/java/com/example/config/ElasticSearchConfig.java
@@ -3,6 +3,9 @@ package com.example.config;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
@@ -11,26 +14,45 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+
 @Configuration
 public class ElasticSearchConfig {
 
-    @Bean	
-    public RestHighLevelClient elasticsearchClient() {
-      
+    @Bean
+    public RestHighLevelClient elasticsearchClient() throws Exception {
+
+        // 1. 인증 정보 설정
         final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(
                 AuthScope.ANY,
-                new UsernamePasswordCredentials("elastic", "votmdnjem") 
+                new UsernamePasswordCredentials("elastic", "votmdnjem")
         );
-        	//엘라스틱서버 연결객체
+
+        KeyStore truststore = KeyStore.getInstance("PKCS12");
+        try (FileInputStream is = new FileInputStream("D:\\2jo\\moonpull\\eskey\\ca.p12")) {
+            truststore.load(is, "votmdnjem".toCharArray());
+        }
+
+        SSLContext sslContext = SSLContexts.custom()
+                .loadTrustMaterial(truststore, new TrustSelfSignedStrategy())
+                .build();
+        // 3. Elasticsearch 클라이언트 빌더
         RestClientBuilder builder = RestClient.builder(
-                new HttpHost("34.64.215.144", 9200, "http"))			//엘라스틱에게 http세팅
-                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
-                    @Override //엘라스틱서치 비동기로 보낸것(여러개 동시 처리때문에)
-                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
-                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-                    }
-                });
+                new HttpHost("34.64.151.197", 9200, "https"), // node-1
+                new HttpHost("34.64.215.144", 9200, "https"), // node-2
+                new HttpHost("34.64.84.23", 9200, "https")    // node-3
+        ).setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+            @Override
+            public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                return httpClientBuilder
+                        .setSSLContext(sslContext)
+                        .setDefaultCredentialsProvider(credentialsProvider);
+            }
+        });
 
         return new RestHighLevelClient(builder);
     }
