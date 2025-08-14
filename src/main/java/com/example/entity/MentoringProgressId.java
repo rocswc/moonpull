e8086c32988714e8c26e32e0fe4876cdcc53a76a
billy4242 <billy4242@naver.com>
2025-08-14 13:12:51 +0900

    dd

---
+++ src/main/java/com/example/entity/MentoringProgressId.java
@@ -0,0 +1,15 @@
+package com.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MentoringProgressId implements Serializable {
+    private int menteeId;
+    private int mentorId;
+}
