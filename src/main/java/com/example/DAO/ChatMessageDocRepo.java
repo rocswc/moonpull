f45e627036d00232f193d1ce02f629b1480b1aa1
angelkkj1169 <kkjspdlqj@naver.com>
2025-08-18 17:36:49 +0900

    커밋

---
+++ src/main/java/com/example/DAO/ChatMessageDocRepo.java
@@ -0,0 +1,14 @@
+package com.example.DAO;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.example.VO.ChatMessage;
+
+import java.time.Instant;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ChatMessageDocRepo extends MongoRepository<ChatMessage, String> {
+	  Page<ChatMessage> findByChatroomIdOrderByCreatedAtDesc(long roomId, Pageable pageable);
+	  Page<ChatMessage> findByChatroomIdAndCreatedAtLessThanOrderByCreatedAtDesc(long roomId, Instant before, Pageable pageable);
+}
