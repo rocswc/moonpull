d71997ed3342ffced7e93ee622af1d5081152b1b
billy4242 <billy4242@naver.com>
2025-08-20 09:58:33 +0900

    Merge branch 'master' of https://github.com/rocswc/moonpull.git

---
+++ src/main/java/com/example/service/MyMentorListService.java
@@ -0,0 +1,27 @@
+package com.example.service;
+
+import com.example.DAO.MyMentorListRepository;
+import com.example.dto.MyMentorListDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MyMentorListService {
+
+    private final MyMentorListRepository repository;
+
+    public MyMentorListService(MyMentorListRepository repository) {
+        this.repository = repository;
+    }
+
+    // 진행중 멘토링 불러오기2
+    public List<MyMentorListDTO> getActiveMentorings(int menteeId) {
+        return repository.findByMenteeId(menteeId);
+    }
+
+    // 멘토링 종료
+    public int endMentoring(int progressId) {
+        return repository.endMentoring(progressId);
+    }
+}
